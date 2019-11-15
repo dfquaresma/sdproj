8c7547900f1dba7d220a2b6b5f8efb90b7e6d8a7
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-11-14 22:20:43 -0300

    Add listfiller functions

---
+++ listfiller-nogci-func/listfiller-nogci/src/main/java/com/openfaas/function/Handler.java
@@ -0,0 +1,27 @@
+package com.openfaas.function;
+
+import com.openfaas.model.IHandler;
+import com.openfaas.model.IResponse;
+import com.openfaas.model.IRequest;
+import com.openfaas.model.Response;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Handler implements com.openfaas.model.IHandler {
+
+    public IResponse Handle(IRequest req) {
+        callFunction();
+        Response res = new Response();
+	    res.setBody("Done");
+	    return res;
+    }
+
+    public void callFunction() {
+        int size = (int) Math.pow(2, 21);
+        List<Integer> list  = new ArrayList();
+        for (int i = 0; i < size; i++) {
+                list.add(i);
+        }
+    }
+}
