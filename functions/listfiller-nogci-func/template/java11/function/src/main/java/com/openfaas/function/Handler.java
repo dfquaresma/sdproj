f88de81e602a2f5106db620f87910862ffb099e6
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-11-14 22:30:09 -0300

    Update functions

--- functions/listfiller-nogci-func/template/java11/function/src/main/java/com/openfaas/function/Handler.java
+++ functions/listfiller-nogci-func/template/java11/function/src/main/java/com/openfaas/function/Handler.java
@@ -5,23 +5,12 @@ import com.openfaas.model.IResponse;
 import com.openfaas.model.IRequest;
 import com.openfaas.model.Response;
 
-import java.util.List;
-import java.util.ArrayList;
-
 public class Handler implements com.openfaas.model.IHandler {
 
     public IResponse Handle(IRequest req) {
-        callFunction();
         Response res = new Response();
-	    res.setBody("Done");
-	    return res;
-    }
+	    res.setBody("Hello, world!");
 
-    public void callFunction() {
-        int size = (int) Math.pow(2, 21);
-        List<Integer> list  = new ArrayList();
-        for (int i = 0; i < size; i++) {
-                list.add(i);
-        }
+	    return res;
     }
 }
