ea2dccb45889be8a10a4e3c7504f1aae7a22eddb
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-11-16 16:06:02 -0300

    Update handlers of functions

--- functions/listfiller-gci-func/listfiller-gci/src/main/java/com/openfaas/function/Handler.java
+++ functions/listfiller-gci-func/listfiller-gci/src/main/java/com/openfaas/function/Handler.java
@@ -18,7 +18,7 @@ public class Handler implements com.openfaas.model.IHandler {
     }
 
     public void callFunction() {
-        int size = (int) Math.pow(2, 21);
+        int size = (int) Math.pow(2, 15);
         List<Integer> list  = new ArrayList();
         for (int i = 0; i < size; i++) {
                 list.add(i);
