a8374081a015bf7ae0a70841857e2699adf2d3a0
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-11-17 12:24:17 -0300

    Update seq-workload.sh

--- experiment/seq-workload.sh
+++ experiment/seq-workload.sh
@@ -22,11 +22,28 @@ fi
 for flag in ${FLAGS};
 do
     FILE_NAME="${RESULTS_PATH}${FUNC}-${flag}${EXPID}.csv"
-    echo -e "status;latency" > ${FILE_NAME}
+    echo -e "id;status;latency" > ${FILE_NAME}
     for i in `seq 1 ${REQS}`
     do
-        curl -X GET -o /dev/null -s -w '%{http_code};%{time_total}\n' "${TARGET}${FUNC}-${flag}" >> ${FILE_NAME}
+        curl_return=$(curl -X GET -o /dev/null -s -w '${i};%{http_code};%{time_total}\n' ${TARGET}${FUNC}-${flag})
+        check=$(echo ${curl_return} | grep -v ";200;" | wc -l)
+        if [ $check -gt 0 ]; 
+            then
+                TAG=${RESULTS_PATH}${FUNC}-${flag}${EXPID}
+                container=$(sudo docker ps -f name=listfiller-${flag} --format "{{.ID}}")
+                for id in ${container}; 
+                    do 
+                        docker cp "${id}:/home/app/gc.log" "${TAG}-gc-${id}.log"
+                        docker cp "${id}:/home/app/proxy-stdout.log" "${TAG}-proxy-stdout-${id}.log"
+                        docker cp "${id}:/home/app/proxy-stderr.log" "${TAG}-proxy-stderr-${id}.log"
+                        docker logs ${id} >${TAG}-stdout-${id}.log 2>${TAG}-stderr-${id}.log
+                    done
+                exit
+            else
+                echo ${curl_return} >> ${FILE_NAME}
+        fi
     done
     sed -i 's/,/./g' ${FILE_NAME}
     sed -i 's/;/,/g' ${FILE_NAME}
 done
+for f in $(docker service ps -q $service);do docker inspect --format '{{.NodeID}} {{.Status.ContainerStatus.ContainerID}}' $f; done
