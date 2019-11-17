9358e2d2ad52f522272ea0f7baf1cf3dd55948af
David Ferreira Quaresma <davidferreiraquaresma@gmail.com>
2019-11-17 00:12:06 -0300

    Update seq-run to teardown and setup correctly

--- experiment/seq-run.sh
+++ experiment/seq-run.sh
@@ -14,6 +14,13 @@ mkdir -p ${RESULTS_PATH}
 for expid in `seq 1 ${NUMBER_OF_EXPERIMENTS}`;
 do
     # to tear down old containeres
-    sudo docker ps --filter name=listfiller* -aq | sudo xargs docker stop | sudo xargs docker rm
+    sudo faas-cli remove listfiller-gci
+    cd ../functions/listfiller-gci-func/
+    sudo faas-cli up -f listfiller-gci.yml
+    sudo docker service update --publish-add published=31123,target=8080 listfiller-gci
+    sudo faas-cli remove listfiller-nogci
+    cd ../listfiller-nogci-func/
+    sudo faas-cli up -f listfiller-nogci.yml
+    cd ../../experiment
     EXPID=${expid} RESULTS_PATH=${RESULTS_PATH} bash ./seq-workload.sh
 done
