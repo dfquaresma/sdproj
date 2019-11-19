15269912f6f163aa61a78b4928f7efc2457c7073
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-11-19 19:21:53 -0300

    Update experiment run scripts and gci results

--- experiment/seq-run.sh
+++ experiment/seq-run.sh
@@ -11,6 +11,7 @@ echo "RESULTS_PATH: ${RESULTS_PATH:=./results/}"
 echo "NUMBER_OF_EXPERIMENTS: ${NUMBER_OF_EXPERIMENTS:=1}"
 echo "NUMBER_OF_EXPERIMENTS: ${REQS:=1000}"
 
+sleep 5
 mkdir -p ${RESULTS_PATH}
 for expid in `seq 1 ${NUMBER_OF_EXPERIMENTS}`;
 do
