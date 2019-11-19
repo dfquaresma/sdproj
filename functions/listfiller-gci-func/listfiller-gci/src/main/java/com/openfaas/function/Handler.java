db4ad854677c8bff34ac92b9981ef071b6c9385b
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-11-19 14:46:08 -0300

    Update functions

--- functions/listfiller-gci-func/listfiller-gci/src/main/java/com/openfaas/function/Handler.java
+++ functions/listfiller-gci-func/listfiller-gci/src/main/java/com/openfaas/function/Handler.java
@@ -9,11 +9,12 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Handler implements com.openfaas.model.IHandler {
+    private int count;
 
     public IResponse Handle(IRequest req) {
         callFunction();
         Response res = new Response();
-	    res.setBody("ListFiller GCI! :)");
+	    res.setBody("nogci:" + count++);
 	    return res;
     }
 
